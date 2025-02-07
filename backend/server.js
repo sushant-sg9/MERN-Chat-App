9beb1379832a670f6ddaaa53e7b53f94baec0619
Sushant <sushantghadge2016@gmail.com>
2025-02-07 20:10:46 +0530

    chat app

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,10 @@ app.use('/api/message',messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build"))); // Adjust path
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "../frontend/build", "index.html")) // Adjust path
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
